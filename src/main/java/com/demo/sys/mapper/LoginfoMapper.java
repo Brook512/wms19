5bed8beb61501f95cc424508984b4c70a0d5f9a5
FanDe1 <1579149542@qq.com>
2024-12-21 14:21:18 +0800

    实现账户登录界面和功能

---
+++ src/main/java/com/demo/sys/mapper/LoginfoMapper.java
@@ -0,0 +1,8 @@
+package com.demo.sys.mapper;
+
+import com.demo.sys.entity.Loginfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface LoginfoMapper extends BaseMapper<Loginfo> {
+
+}
---
+++ src/main/java/com/demo/sys/service/ILoginfoService.java
@@ -0,0 +1,9 @@
+package com.demo.sys.service;
+
+import com.demo.sys.entity.Loginfo;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+
+public interface ILoginfoService extends IService<Loginfo> {
+
+}
