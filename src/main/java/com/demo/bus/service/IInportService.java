be2912d87f287be747d42783dd3af320b4a68765
L3072292093 <3072292093@qq.com>
2024-12-26 21:45:52 +0800

    完成仓库系统出入库功能

---
+++ src/main/java/com/demo/bus/service/IInportService.java
@@ -0,0 +1,8 @@
+package com.demo.bus.service;
+
+import com.demo.bus.entity.Inport;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface IInportService extends IService<Inport> {
+
+}
